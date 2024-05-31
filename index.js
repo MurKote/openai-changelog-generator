71269320ce8fc91716e6ab41ff3499dd8b76e260
Mike Bronner <mike@genealabs.com>
2024-05-31 13:28:41 -0700

    feat: ✨ Fine-tuned openAI prompt to provide expected results.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const _ = require("lodash");
 const core = require("@actions/core");
 const { OpenAI } = require('openai');
+const github = require("@actions/github");
 
 const changes = core.getInput("changes");
 const openApiKey = core.getInput("openai-api-key");
 const openAi = new OpenAI({apiKey: openApiKey});
+const payload = github.context.payload;
+const repositoryOwner = payload.repository.owner.login;
+const repositoryName = payload.repository.name;
 
 process();
 
 async function process() {
+  const prompt = buildPrompt();
+  console.log(prompt);
     const parameters = OpenAi.Chat.ChatCompletionCreateParams = {
         messages: [{
                 role: "system",
-                content: this.buildPrompt(),
+                content: prompt,
             },
             {
                 role: "user",
@@ -28,8 +34,7 @@ async function process() {
 
 function buildPrompt() {
     return `
-Generate a changelog for the web version of the PhotoRoom app, which offers AI-driven image editing
-capabilities such as background removal, retouching, resizing, and more, detailing recent updates.
+Generate a changelog based on github commits using the Conventional Commit standard with gitmojis.
 
 The changelog should:
 
@@ -37,13 +42,17 @@ The changelog should:
   informal.
 2. be informative: using the provided list of GitHub commits which follow the conventional commit
   standard, break them down into categories such as Features, Bug Fixes, etc. Each category should
-  be a leavel 2 heading preceeded by an emoji following the gitmoji standard.
-3. list each commit in the category it belongs to, based on the conventional commit standard.
-  Remove the gitmoji prefix from the commit message. Also remove any trailing github reference links.
+  be a leavel 2 heading preceeded by an emoji following the gitmoji standard. Do not list categories
+  that do not have any commits.
+3. list each commit in the category it belongs to, based on the conventional commit standard. Commits
+  that do not follow the convention commit standard should be listed at the end in an "Other" category.
+  Each listing should read as a complete sentence.
 4. summarize the achievements of this release in a manner that is easily understood by non-technical
   readers before listing the commits.
 5. use Markdown formatting.
-6. use the following syntax to create links: ``[http://www.example.com](This message is a link)``.
+6. use the following syntax to create links: ``[http://www.example.com](#12345)``. Links should
+  point to GitHub pull requests and only show the pull request number prefixed with ``#`` for the github
+  repository ``${repositoryOwner}/${repositoryName}``.
 7. not wrap the answer in a codeblock. Just output the text, nothing else. Here's a good
   example to follow, please try to match the formatting as closely as possible, only changing
   the content of the changelog and have some liberty with the introduction and conclusion
@@ -55,12 +64,7 @@ The changelog should:
  And here's an example of the full changelog:
   ````
   # Release 1.2.3 (2022-03-25)
-  The PhotoRoom web team has been working diligently to bring you the latest updates and
-  improvements to enhance your editing experience. We are thrilled to share the following changes
-  with you. Let's dive in!  🤿
-
-  These updates wouldn't be possible without the hard work and dedication of our incredible PhotoRoom web team. We are grateful for their commitment to delivering a top-notch editing experience for our users. Wishing everyone a delightful and restful weekend!
-  Stay tuned for more exciting updates coming soon!
+  This release includes crucial bug fixes and ongoing work to enhance our release automation process. We addressed issues with Bugsnag error grouping and fixed repeated calls to cancel multipart uploads, improving overall stability and reliability. Additionally, we are testing new automation features to streamline future releases.
 
   ## ✨ Features
   - We optimize our ci to strip comments and minify production builds. ([https://github.com/facebook/react/pull/27304/](#27304))
@@ -90,7 +94,7 @@ The changelog should:
   ## 🔁 Continuous Integration
   - We optimize our ci to strip comments and minify production builds. ([https://github.com/facebook/react/pull/27304/](#27304))
 
-  ## 📝 Chores
+  ## 🧹 Chores
   - We optimize our ci to strip comments and minify production builds. ([https://github.com/facebook/react/pull/27304/](#27304))
 
   ## ⏪️ Reverts
