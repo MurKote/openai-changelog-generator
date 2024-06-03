7f42ee54dd0ef374010fdf5edc3f0511cf556a6a
Mike Bronner <mike@genealabs.com>
2024-06-03 07:48:27 -0700

    WIP

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function process() {
     //     model: "gpt-4o",
     // };
     const chatCompletion = await openAiChat.chat.completions.create({
-        model: "gpt-4-32k",
+        model: "gpt-3.5-turbo",
         prompt: buildPrompt(),
         max_tokens: 50,
     });
